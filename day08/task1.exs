ae4b9f6814aa46337b8abb2557f2504425353a34
Vasil Joskov <v.joskov@xs-software.com>
2016-12-09 02:13:48 +0200

    Day 8 Task 1

---
+++ day08/task1.exs
@@ -0,0 +1,56 @@
+defmodule Task do
+  def iterate_lines([], result), do: result
+  def iterate_lines([head | tail], result) do
+    result = apply_operation(head, result)
+    iterate_lines(tail, result)
+  end
+
+  def apply_operation("rect " <> rest, result) do
+    res = Regex.run(~r/(\d+)x(\d+)/, rest, capture: :all_but_first)
+    [w, h] = Enum.map(res, &String.to_integer/1)
+    added = for x <- 0..(w - 1), y <- 0..(h - 1), do: {x, y}
+    Enum.uniq(added ++ result)
+  end
+  def apply_operation("rotate row " <> rest, result) do
+    res = Regex.run(~r/y=(\d+) by (\d+)/, rest, capture: :all_but_first)
+    [y, by] = Enum.map(res, &String.to_integer/1)
+    shift_y(result, [], y, by)
+  end
+  def apply_operation("rotate column " <> rest, result) do
+    res = Regex.run(~r/x=(\d+) by (\d+)/, rest, capture: :all_but_first)
+    [x, by] = Enum.map(res, &String.to_integer/1)
+    shift_x(result, [], x, by)
+  end
+
+  def shift_x([], result, _x, _by), do: result
+  def shift_x([head | tail], result, x, by) do
+    result = [shift_elem_x(head, x, by) | result]
+    shift_x(tail, result, x, by)
+  end
+
+  def shift_elem_x({x, y}, x, by), do: {x, rem(y + by, 6)}
+  def shift_elem_x(result, _x, _by), do: result
+
+  def shift_y([], result, _y, _by), do: result
+  def shift_y([head | tail], result, y, by) do
+    result = [shift_elem_y(head, y, by) | result]
+    shift_y(tail, result, y, by)
+  end
+
+  def shift_elem_y({x, y}, y, by), do: {rem(x + by, 50), y}
+  def shift_elem_y(result, _y, _by), do: result
+
+  def calculate(input) do
+    input
+      |> String.split("\n")
+      |> List.delete_at(-1)
+      |> iterate_lines([])
+  end
+end
+
+{:ok, input} = File.read("input.txt")
+IO.puts input
+
+result = Task.calculate(input)
+IO.inspect result
+IO.puts length(result)
